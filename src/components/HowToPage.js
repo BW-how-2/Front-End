aaaf8ecc7b6827baed8190411601ba1db0d239cc
christiano39 <carneson39@gmail.com>
2020-07-28 14:33:45 -0600

    fixed Login

--- src/components/HowToPage.js
+++ src/components/HowToPage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { HowToContext } from '../contexts/HowToContext'
 import { useParams } from 'react-router-dom'
 
@@ -6,18 +6,19 @@ export default function Item() {
     const { howTos } = useContext(HowToContext)
     const { howtoID } = useParams() 
     const howTo = howTos.find(id => {
+        debugger
         return id.id === howtoID
     })
 
 return (
     <div className='howto-wrapper'>
-        <div className='howto-header'>
+        {howTo && <div className='howto-header'>
             <h2>{howTo.name}</h2>
             <h3>{howTo.description}</h3>
                 <h3>{howTo.category}</h3>
                 <h3>{howTo.complexity}</h3>
                 <p>{howTo.steps}</p>
-        </div>
+        </div> }
     </div>
 )
 
