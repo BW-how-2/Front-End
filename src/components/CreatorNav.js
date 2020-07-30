0d95645055a152bbb7e0c986696fb6cf93330898
christiano39 <carneson39@gmail.com>
2020-07-30 11:14:32 -0600

    styled creator nav bar

--- src/components/CreatorNav.js
+++ src/components/CreatorNav.js
@@ -5,18 +5,32 @@ import styled from 'styled-components';
 const StyledCreatorNav = styled.div`
     display: flex;
     justify-content: space-evenly;
+    margin: 15px auto;
 
     a {
         color: inherit;
+    }
+
+    .nav-link {
+        background-color: white;
+        border: 1px solid white;
+        padding: 5px 10px;
+        border-radius: 10px;
+        color: black;
+        box-shadow: 0 1.5px 5px lightgrey;
+        font-weight: 500;
+        text-decoration: none;
+        letter-spacing: 2px;
+        font-size: 14px;
     }
 `
 
 const CreatorNav = () => {
     return (
         <StyledCreatorNav>
-            <NavLink to='/dashboard' activeClassName='active'>User Dashboard</NavLink>
-            <NavLink to='/dashboard/creator' activeClassName='active'>Creator Dashboard</NavLink>
-            <NavLink to='/userList' activeClassName='active'>Users</NavLink>
+            <NavLink className='nav-link' to='/dashboard'>User Dashboard</NavLink>
+            <NavLink className='nav-link' to='/dashboard/creator'>Creator Dashboard</NavLink>
+            <NavLink className='nav-link' to='/userList'>Users</NavLink>
         </StyledCreatorNav>
     )
 }
