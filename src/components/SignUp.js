d8a648cf8a4062eda4c72a2a7711ddf406fe83cd
Jabin Washburn <jabinwashburn@gmail.com>
2020-07-30 10:20:58 -0700

    got form validation working just need to get styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,26 +1,57 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Axios from 'axios' 
 import {UserContext} from '../contexts/UserContext'
 import {useHistory} from 'react-router-dom'
+import * as Yup from 'yup'
 
 const initialForm={
     username: '',
     password: '',
-    role: ''
+    role: 'user'
 }
 
+const formSchema = Yup.object().shape({
+    name: Yup
+    .string()
+    .min(2, 'Please use more characters')
+    .required('Please use more characters'),
+    password: Yup
+    .string()
+    .min(3, 'Please add your password')
+    .required('Password is required'),
+})
 
 
 export default function SignUp(){
 
     const [signUp, setSignUp] = useState(initialForm)
+    const [disable, setDisable] = useState([])
+    const [errors, setErrors] = useState(initialForm)
 
     const {user, setUser} = useContext(UserContext)
 
     const history = useHistory()
 
+    useEffect(() => {
+        formSchema.isValid(signUp).then((valid) => {
+            setDisable(!valid)
+        })
+    }, [signUp])
+
+    const validForm = (e) => {
+        Yup
+        .reach(formSchema, 'name')
+        .validate(e.target.value)
+        .then(valid => {
+            setErrors({...errors, [e.target.name]: ''})
+        })
+        .catch(err => setErrors({...errors, [e.target.name]: err.errors}))
+    }
+
     const handleChange = (e) => {
+        e.persist()
         setSignUp({...signUp, [e.target.name]: e.target.value})
+        validForm(e)
     }
 
     const handleSubmit = (e) => {
@@ -52,9 +83,11 @@ export default function SignUp(){
         <form onSubmit={handleSubmit}>
             <h1>Please Register</h1>
                 <p> Please type a username:</p>
+                <p className='error'>{errors.username}</p>
                 <input id='username' name='username' value={signUp.username} onChange={handleChange}></input> 
 
                 <p>Please type a password:</p>
+                <p className='error'>{errors.password}</p>
                 <input type='password' id='password' name='password' value={signUp.password} onChange={handleChange}></input>
 
                 <p>Please select a role:</p>
