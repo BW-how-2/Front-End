de1d84f72d8f4c2546a5a846d685d5c98abb00d4
haileyhansard <haileyhansard@gmail.com>
2020-07-30 09:08:29 -0700

    incorporating styling colors

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -36,7 +36,7 @@ const StyledListOfUsers = styled.div`
     align-items: center;
     width: 100%;
     margin: 4% 20%;
-    background-color: #EDF4F5;
+    /* background-color: rgba(240, 240, 240, 0.822); */
 
     .user-items{
         display: flex;
@@ -53,7 +53,7 @@ const Button = styled.button`
     border-radius: 5px;
 
     &:hover{
-        background-color: orange;
+        background-color: #FCA311;
         color: white;
     }
 `;
